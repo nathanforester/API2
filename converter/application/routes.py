1be15c24206a7d1cc7db992913709a52d9cb2ee9
nathanforester <nfeugene86@gmail.com>
2022-08-02 12:49:14 +0100

    first commit

---
+++ converter/application/routes.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, Response, message_flashed 
+import sys
+from flask import jsonify
+from flask import jsonify
+import json
+from flask import json
+
+
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+
+@app.route('/date/<birthDate>', methods=['GET', 'POST'])
+def birthDate(birthDate):
+    try:
+        birthDate = int(float(birthDate))
+    except ValueError:
+        return "ValueError: enter a number"
+    year = datetime.utcnow().year
+    ageInMonths = (int(year) - int(birthDate)) * 12
+    if birthDate < 1:
+        return 'value entered is less than one month'
+    elif birthDate > 2025:
+        return 'value exceeds current birth year'
+    else: 
+        return str(ageInMonths)
+    
